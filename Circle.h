a07192ae802b8aa93b67d4a5c71fb07ae7fe1f69
kaisei-tanita05 <k024g0081@g.neec.ac.jp>
2025-11-11 13:04:43 +0900

    抽象クラスとインターフェース実装

---
+++ Circle.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "IShape.h"
+
+#pragma once
+
+#include "IShape.h"
+
+class CIRCLE : public IShape {
+public:
+	void Size()override;
+	void Draw()override;
+private:
+	float answer = 0;
+};
+
